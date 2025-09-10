cb5d1b4234de4c0bc12f83df9abd4e27447a458d
elvisleyva49 <elvisleyva49@gmail.com>
2025-09-10 12:46:52 -0500

    Login

---
+++ EXUI_Leyva/EXUI_Leyva/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using EXUI_Leyva.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EXUI_Leyva.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        ClsLogin objLogin = new ClsLogin();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string usuario, string clave)
+        {
+            string resultado = objLogin.Validar(usuario, clave);
+
+            if (resultado == "Acceso Exitoso")
+            {
+                ViewBag.Mensaje = "Acceso exitoso";
+            }
+            else
+            {
+                ViewBag.Mensaje = resultado;
+                return View();
+            }
+
+            return View();
+        }
+    }
+}
---
+++ EXUI_Leyva/EXUI_Leyva/Models/ClsLogin.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EXUI_Leyva.Models
+{
+    public class ClsLogin
+    {
+        public string Usuario { get; set; }
+        public string Clave { get; set; }
+
+        public static string[] usuarios = { "elvis", "lanchipa" };
+        public static string[] claves = { "123", "123" };
+        public static string[] estados = { "activo", "activo" };
+
+        public static int intentos = 0;
+
+        public string Validar(string usuario, string clave)
+        {
+
+            if (usuario == "" || clave == "")
+            {
+                return "Los campos no deben estar vacios";
+            }
+
+            for (int i = 0; i < usuarios.Length; i++)
+            {
+                if (usuarios[i] == usuario)
+                {
+                    if (claves[i] == clave)
+                    {
+                        if (estados[i] == "activo")
+                        {
+                            intentos = 0;
+                            return "Acceso Exitoso";
+                        }
+                        else
+                        {
+                            return "Usuario desactivado";
+                        }
+                    }
+
+                }
+            }
+            intentos++;
+            if (intentos >= 3)
+            {
+                return "Superaste el límite de 3 intentos";
+            }
+            else
+            {
+                return "Clave o usuario incorrecto";
+            }
+        }
+
+    }
+}
